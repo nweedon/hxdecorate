0ed1d03965a0b720f8a0357ae30d8f0f39cb60b7
nweedon <niallweedon@gmail.com>
2016-01-23 14:54:39 +0000

    Try using TRAVIS_BUILD_DIR and update URL to io.js

--- travis/setup/node-setup.sh
+++ travis/setup/node-setup.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 set -e
 
-curl https://iojs.org/dist/latest/iojs-v3.3.0-linux-x64.tar.gz > iojs.tar.gz
+curl https://iojs.org/dist/v3.3.0/iojs-v3.3.0-linux-x64.tar.gz > iojs.tar.gz
 tar -zxvf iojs.tar.gz -C ~
 export PATH=$PATH:~/iojs-v3.3.0-linux-x64/bin
 chmod +x ~/iojs-v3.3.0-linux-x64/bin/iojs
